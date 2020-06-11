a7b0681ed672b5f95ba2fc734fe6d7558ba4b7ab
Roger Rezende <roger.rezendegmail.com>
2020-06-11 16:10:45 -0300

    configuração do given, when e then do consulta_classificacao_alemao.py

--- features/steps/consulta_classificacao_alemao.py
+++ features/steps/consulta_classificacao_alemao.py
@@ -3,3 +3,23 @@ from behave import given, when, then
 
 # variável que irá receber a URL do site que iremos interagir
 base_url = 'http://globoesporte.globo.com/'
+
+# configuração do step given
+@given(u'acesso a página inicial do Globoesporte')
+def step_impl(context):
+	raise NotImplementedError(u'STEP: Given acesso a página inicial do Globoesporte')
+
+# configuração do primeiro step when
+@when(u'clico no menu do alemão')
+def step_impl(context):
+	raise NotImplementedError(u'STEP: When clico no menu do brasileirão')
+
+# configuração do segundo step when
+@when(u'classificação é exibida')
+def step_impl(context):
+	raise NotImplementedError(u'STEP: When classificação é exibida')
+
+# configuração do step then
+@then(u'devo saber quem é o último colocado')
+def step_impl(context):
+	raise NotImplementedError(u'STEP: Then devo saber quem é o primeiro colocado')
