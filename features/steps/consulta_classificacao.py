21c3c6919cc2669bebf3c62da3b1a53865c5cb22
Roger Rezende <roger.rezendegmail.com>
2020-06-11 14:09:29 -0300

    configuração inicial do consulta_classificacao.py

---
+++ features/steps/consulta_classificacao.py
@@ -0,0 +1,5 @@
+# importação do given, when e then do behave
+from behave import given, when, then
+
+# variável que irá receber a URL do site que iremos interagir
+base_url = 'http://globoesporte.globo.com/'
