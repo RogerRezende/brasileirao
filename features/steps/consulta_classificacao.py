6c7bb6fb1bdd4b41de96e17261d191a850be8520
Roger Rezende <roger.rezendegmail.com>
2020-06-11 14:14:41 -0300

    configuração do step given em consulta_classificacao.py

--- features/steps/consulta_classificacao.py
+++ features/steps/consulta_classificacao.py
@@ -3,3 +3,10 @@ from behave import given, when, then
 
 # variável que irá receber a URL do site que iremos interagir
 base_url = 'http://globoesporte.globo.com/'
+
+# configuração do step given
+@given(u'acesso a página inicial do Globoesporte')
+def step_impl(context):
+	raise NotImplementedError(u'STEP: Given acesso a página inicial do Globoesporte')
+
+
